94b1d46a683a4021fd8241ad590dc53d1602c358
JHenriqueOli <joaoinacio471@gmail.com>
2025-04-08 00:03:05 -0300

    nn sei

--- JavaScraipt/aula12ex/ex.015/scrript.js
+++ JavaScraipt/aula12ex/ex.015/scrript.js
@@ -1,13 +1,11 @@
-function verificar(){
+function calcular() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.ariaValueMax.length == 0 || fano.value > ano) {
-        window.alert(' [ERRO] verifique os dados e tente novamente!')
+    if (fano.value.length == 0 || fano. value > ano) {
+        window.alert('[ERRO] digite as informações denovo.')
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        window.alert('Tudo Ok!')
     }
 }
