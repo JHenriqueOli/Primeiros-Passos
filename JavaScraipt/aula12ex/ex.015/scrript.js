2d80066f92214ee70feb01a60d9dc2d1ef57d365
JHenriqueOli <joaoinacio471@gmail.com>
2025-04-08 23:33:34 -0300

    arrumei

--- JavaScraipt/aula12ex/ex.015/scrript.js
+++ JavaScraipt/aula12ex/ex.015/scrript.js
@@ -1,11 +1,52 @@
-function calcular() {
+function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano. value > ano) {
-        window.alert('[ERRO] digite as informações denovo.')
+
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] digite as informações de novo.')
     } else {
-        window.alert('Tudo Ok!')
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'menino.png')
+            }else if (idade >= 10 && idade < 21){
+                //jovem
+                img.setAttribute('src', 'homem.png')
+            }else if (idade >= 21 && idade < 50){
+                //adulto
+                img.setAttribute('src', 'homem.png')
+            }else if (idade >= 50){
+                //idoso
+                img.setAttribute('src', 'vei.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10){
+                //criança
+                img.setAttribute('src', 'menina.png')
+            }else if (idade >= 10 && idade < 21){
+                //jovem
+                img.setAttribute('src', 'mulher.png')
+            }else if (idade >= 21 && idade < 50){
+                //adulto
+                img.setAttribute('src', 'mulher.png')
+            }else if (idade >= 50){
+                //idoso
+                img.setAttribute('src', 'veia.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
